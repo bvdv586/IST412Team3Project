3ce85bf3c826cb847662d8bbb560d89d48f0f032
Brendan <bvdv586@gmail.com>
2022-11-09 16:02:53 -0500

    Chnaged project base to a framework to align more with studied programs.

---
+++ ISTTeam3Final/ISTTeam3Final/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ISTTeam3Final.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ISTTeam3Final/ISTTeam3Final/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ISTTeam3Final.Startup))]
+namespace ISTTeam3Final
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
